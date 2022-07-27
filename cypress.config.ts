4760ab696ad0a7d0927f126de3aabe9ad8f35dd1
supriya-kotturu <supriyakotturu@gmail.com>
2022-07-27 12:44:31 +0530

    feat: :white_check_mark: add cypress record

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+	projectId: 'qmiutk',
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
+		baseUrl: 'http://localhost:3030',
 	},
 
 	component: {
---
+++ cypress/component/TestComponent.cy.ts
@@ -0,0 +1,5 @@
+describe('TestComponent.cy.ts', () => {
+  it('playground', () => {
+    // cy.mount()
+  })
+})
